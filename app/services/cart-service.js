60db82749273525bef73a8979afbd0c9e0191ec9
Christian Gruber <christian.gruber@check24.de>
2016-03-18 16:39:03 +0100

    Use real cart id

--- app/services/cart-service.js
+++ app/services/cart-service.js
@@ -1,11 +1,10 @@
 import serviceDiscovery from './service-discovery';
 
-export default {
-  currentCartId: 34534,
+let currentCartId = undefined;
 
+export default {
   addItem: (id) => {
-    console.log('this.currentCartId', this.currentCartId);
-    if (!this.currentCartId) {
+    if (!currentCartId) {
       console.log("Trying to post to cart service with id", id);
 
       const cartClient = serviceDiscovery.getClient('cart-service');
@@ -14,17 +13,19 @@ export default {
           .post('/carts')
           .send({ 'itemId': id, count: 1 })
           .end(function (err, res) {
-            if (res.text && res.text.length) {
-              //const cartId = JSON.stringify(res.text.cartId);
-              //console.log('done posting to cart service.', cartId);
-              //resolve(cartId);
-              this.currentCartId = 123456;
-              resolve(12345)
+            if (res.body) {
+              const cartId = res.body.cartId;
+              console.log('done posting to cart service.', cartId);
+              resolve(cartId);
+              // currentCartId = 123456;
+              // resolve(12345)
             } else {
 
             }
           });
         });
+      } else {
+        return Promise.resolve(currentCartId);
       }
     }
 }
