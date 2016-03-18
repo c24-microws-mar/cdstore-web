fc02900b84e9f410c2801e6b7b034eec937f5002
leidy <guitarlady86@gmail.com>
2016-03-18 11:32:47 -0500

    Pass the cart_id at the post notification service

--- app/views/order-button-view.js
+++ app/views/order-button-view.js
@@ -7,12 +7,13 @@ export default {
   render: (baseElement, cartId) => {
     console.log('cartId', cartId);
     $('<button>Order and enjoy!</button>')
-      .click(OnOrderButton.bind(cartId))
+      .click({cartId: cartId}, OnOrderButton)
       .appendTo(baseElement)
   }
 }
 
-function OnOrderButton(event, cartId) {
+function OnOrderButton(event) {
+  const cardId = event.data.cartId;
   notificationService
     .sendNotification(person, cartId)
     .then(() => {
