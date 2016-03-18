7c7d927f0be777046e00b35e27350dd0e8bb7ab2
Christian Gruber <christian.gruber@check24.de>
2016-03-18 14:56:04 +0100

    Fix tags

--- app/services/service-discovery.js
+++ app/services/service-discovery.js
@@ -2,7 +2,7 @@ import agent from 'multiagent';
 
 export default {
 
-  getClient: (serviceName, tags) => {
+  getClient: (serviceName, tag) => {
     const DISCOVERY_SERVERS = [
       'http://46.101.245.190:8500',
       'http://46.101.132.55:8500',
@@ -17,8 +17,8 @@ export default {
       serviceStrategy: 'randomly'
     };
 
-    if (tags) {
-      options.createConsulRequestPath = serviceName => `/v1/health/service/${encodeURIComponent(serviceName)}?passing=true&tags=${tags}`
+    if (tag) {
+      options.createConsulRequestPath = serviceName => `/v1/health/service/${encodeURIComponent(serviceName)}?passing=true&tag=${tag}`
     }
 
     return agent.client(options);
