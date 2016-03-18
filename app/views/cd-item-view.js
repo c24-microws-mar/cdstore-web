46e800e4436c8a65134e4590bfd8daf606e6ff14
Christian Gruber <christian.gruber@check24.de>
2016-03-18 17:10:28 +0100

    Add covers

--- app/views/cd-item-view.js
+++ app/views/cd-item-view.js
@@ -2,8 +2,14 @@ import $ from 'jquery';
 
 export default {
   render: (baseElement, cd) => {
-    $('<td></td>').text(cd.artist).appendTo(baseElement)
-    $('<td></td>').text(cd.album).appendTo(baseElement)
-    $('<td></td>').text(cd.price).appendTo(baseElement)
+    var imageCell = $('<td class="imageCell"></td>');
+    if (cd.hasCover) {
+      $(`<img class="coverImage" src="${cd.coverUrl}"></img>`).appendTo(imageCell);
+    }
+
+    imageCell.appendTo(baseElement);
+    $('<td class="itemCell"></td>').text(cd.artist).appendTo(baseElement)
+    $('<td class="itemCell"></td>').text(cd.album).appendTo(baseElement)
+    $('<td class="itemCell"></td>').text(`${cd.price}.00 Euro`).appendTo(baseElement)
   }
 }
